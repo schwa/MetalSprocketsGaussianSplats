d4e83068f8a991b600a09f818f96d25b780177f4
Jonathan Wight <jwight@mac.com>
2025-11-25 05:23:15 +0100

    Fix peer bundle loading.

--- Sources/MetalSprocketsGaussianSplats/Support/Bundle+Extensions.swift
+++ Sources/MetalSprocketsGaussianSplats/Support/Bundle+Extensions.swift
@@ -3,9 +3,21 @@ import MetalCompilerPluginSupport
 
 extension Bundle {
     static var metalSprocketsGaussianSplatShaders: Bundle {
-        guard let bundle = Bundle.module.parentBundle?.childBundle(withSuffix: "MetalSprocketsGaussianSplatShaders") else {
-            fatalError()
+        guard let bundle = Bundle.module.peerBundle(withSuffix: "MetalSprocketsGaussianSplatShaders") else {
+            fatalError("Failed to load MetalSprocketsGaussianSplatShaders bundle. Parent bundle: \(String(describing: Bundle.module.parentBundle)), searched for suffix: MetalSprocketsGaussianSplatShaders")
         }
         return bundle
+    }
+}
+
+extension Bundle {
+    func peerBundle(withSuffix suffix: String) -> Bundle? {
+        let url = bundleURL.deletingLastPathComponent()
+        let fileManager = FileManager()
+        guard let filename = try? fileManager.contentsOfDirectory(atPath: url.path).first(where: { $0.hasSuffix("_\(suffix).bundle") }) else {
+            return nil
+        }
+        let bundleURL = url.appendingPathComponent(filename)
+        return Bundle(url: bundleURL)
     }
 }
