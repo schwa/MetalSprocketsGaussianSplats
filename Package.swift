3a4887ece0943228edcd660a804e5d7cb83fd6ff
Jonathan Wight <jwight@mac.com>
2025-12-18 23:06:57 -0800

    Use latest MetalSprockets

--- Package.swift
+++ Package.swift
@@ -33,7 +33,6 @@ let package = Package(
         .package(url: "https://github.com/schwa/GeometryLite3D", from: "0.1.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.0"),
         .package(url: "https://github.com/schwa/GoldenImage", branch: "main"),
-//        .package(path: "/Users/schwa/Projects/Sharp")
     ],
     targets: [
         .target(
