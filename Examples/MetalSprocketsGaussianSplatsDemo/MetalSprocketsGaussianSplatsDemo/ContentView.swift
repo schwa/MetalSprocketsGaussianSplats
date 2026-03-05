045cbe102898a3a023054019965ec4ef73660df9
Jonathan Wight <jwight@mac.com>
2026-03-05 13:48:18 -0800

    Rename demo to SuperDemo and add MetalSprockets demo app

---
+++ Examples/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalSprocketsGaussianSplatsDemo
+//
+//  Created by Jonathan Wight on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSprocketsGaussianSplatsDemoApp.swift
+//  MetalSprocketsGaussianSplatsDemo
+//
+//  Created by Jonathan Wight on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalSprocketsGaussianSplatsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
