53098acbffee5679f0b100679b66859f012f7486
Jonathan Wight <jwight@mac.com>
2026-03-20 10:20:06 -0700

    Fix swiftlint conditional_returns_on_newline violation

--- Sources/MetalSprocketsGaussianSplats/Spark/SparkSplatRenderPipeline.swift
+++ Sources/MetalSprocketsGaussianSplats/Spark/SparkSplatRenderPipeline.swift
@@ -130,7 +130,9 @@ public struct SparkSplatRenderPipeline: Element {
                 listenerStarted = true
                 nonisolated(unsafe) let sortedIndicesRef = _sortedIndices
                 Task { @MainActor [weak sortManager] in
-                    guard let sortManager else { return }
+                    guard let sortManager else {
+                        return
+                    }
                     let channel = await sortManager.sortedIndicesChannel()
                     for await sort in channel {
                         // Channel will finish when sortManager is deallocated
