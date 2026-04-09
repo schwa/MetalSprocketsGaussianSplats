e3bf97c08e3b8cefa352158fd56b228d4eec6933
Jonathan Wight <jwight@mac.com>
2026-04-09 11:17:53 -0700

    Move ImmersiveToggle to Support.swift

--- Examples/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemo/ContentView.swift
+++ Examples/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemo/ContentView.swift
@@ -38,36 +38,6 @@ struct ContentView: View {
     }
 }
 
-#if os(visionOS)
-struct ImmersiveToggle: View {
-    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
-    @State private var isImmersive = false
-    @State private var isTransitioning = false
-
-    var body: some View {
-        Button(isImmersive ? "Exit Immersive" : "View in Immersive Space") {
-            isTransitioning = true
-            Task {
-                if isImmersive {
-                    await dismissImmersiveSpace()
-                    isImmersive = false
-                } else {
-                    let result = await openImmersiveSpace(id: "SplatImmersive")
-                    if case .opened = result {
-                        isImmersive = true
-                    }
-                }
-                isTransitioning = false
-            }
-        }
-        .disabled(isTransitioning)
-        .buttonStyle(.borderedProminent)
-        .padding()
-        .glassBackgroundEffect()
-    }
-}
-#endif
 
 #else
 import SwiftUI
---
+++ Examples/MetalSprocketsGaussianSplatsDemo/MetalSprocketsGaussianSplatsDemo/Support.swift
@@ -0,0 +1,32 @@
+#if os(visionOS)
+import SwiftUI
+
+struct ImmersiveToggle: View {
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    @State private var isImmersive = false
+    @State private var isTransitioning = false
+
+    var body: some View {
+        Button(isImmersive ? "Exit Immersive" : "View in Immersive Space") {
+            isTransitioning = true
+            Task {
+                if isImmersive {
+                    await dismissImmersiveSpace()
+                    isImmersive = false
+                } else {
+                    let result = await openImmersiveSpace(id: "SplatImmersive")
+                    if case .opened = result {
+                        isImmersive = true
+                    }
+                }
+                isTransitioning = false
+            }
+        }
+        .disabled(isTransitioning)
+        .buttonStyle(.borderedProminent)
+        .padding()
+        .glassBackgroundEffect()
+    }
+}
+#endif
